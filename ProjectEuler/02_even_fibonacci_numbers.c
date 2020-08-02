2ba9f45bf6201da040850e504cb652d53a5ac14b
Luciano F. Gonçalves <luciano.fg.dv@gmail.com>
2020-08-02 17:20:22 -0300

    Three Project Eulers problems added

---
+++ ProjectEuler/02_even_fibonacci_numbers.c
@@ -0,0 +1,31 @@
+/* By considering the terms in the Fibonacci sequence whose values
+ * do not exceed four million, find the sum of the even-valued terms
+ */
+
+#include <stdio.h>
+
+#define SIZE 64
+int fibonacci[SIZE] = {0, 1, 1};
+
+int calc_fibonacci(int nth) {
+    if (!fibonacci[nth])
+        fibonacci[nth] = calc_fibonacci(nth - 1) + calc_fibonacci(nth - 2);
+    return fibonacci[nth];
+}
+
+int main(int argc, char *argv[])
+{
+    int sum = 0;
+    for (int i = 1; i < SIZE; i++) {
+        int tmp = calc_fibonacci(i);
+        if (tmp > 4000000)
+            break;
+        
+        if(tmp%2 != 0)
+            sum += tmp;
+    }
+
+    printf("SUM: %d\n", sum);
+
+    return 0;
+}
