a9256164523651041ee64f93792b7d17c1e70db7
Luciano F. Gonçalves <luciano.fg.dv@gmail.com>
2020-08-03 23:00:45 -0300

    Solution simplified, using a set instead of a list

--- ProjectEuler/04_largest_palindrome_product.py
+++ ProjectEuler/04_largest_palindrome_product.py
@@ -5,19 +5,13 @@ def is_palindrome(number: int):
     n = str(number)
     return n == n[::-1]
 
-palindrome_list = []
+palindrome_set = set([])
 
 for i in range(100, 999):
     for j in range(i, 999):
         number = i * j
         if is_palindrome(number):
-            palindrome_list.append(number)
+            palindrome_set.add(number)
 
-if len(palindrome_list) > 0:
-    largest = palindrome_list[0]
-    for i in palindrome_list:
-        if i > largest:
-            largest = i
-
-    print(str(largest))
+print(max(palindrome_set))
 
